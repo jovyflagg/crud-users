5f4611d4180a666d0f2dfc479aa5a6222581ae9d
Jovy Cortez <jovycortez11@gmail.com>
2023-06-28 10:33:47 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as
+Router,
+Routes,
+Route } from "react-router-dom";
+import Users from "./components/Users";
+import EditUser from "./components/EditUser";
+
+function App() {
+  return (
+   <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Users />}/>
+        <Route path='/crud-users' element={<Users />}/>
+        <Route path='/edit' element={<EditUser />}/>
+      </Routes>
+    </Router>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Users.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AddUser from './AddUser'
+
+const Users = () => {
+  return (
+    <div><AddUser /></div>
+  )
+}
+
+export default Users
